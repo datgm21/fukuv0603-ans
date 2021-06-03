e98f517b9f0012c235314ec60eead55a36d79fee
A601-00 <tnk.dat.class@gmail.com>
2021-06-03 10:02:45 +0900

    おまけ完成

--- fukuv0603-ans/Form1.cs
+++ fukuv0603-ans/Form1.cs
@@ -22,5 +22,29 @@ namespace fukuv0603_ans
             label1.Left += int.Parse(textBox1.Text);
             label1.Top += int.Parse(textBox2.Text);
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "-5";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "-5";
+            textBox2.Text = "0";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "5";
+            textBox2.Text = "0";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "5";
+        }
     }
 }
